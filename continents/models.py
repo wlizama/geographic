84ddb5c1ecc7b1f854c9e7b357267d4d51bb803d
Wilder Lizama <wilderlizama@gmail.com>
2018-04-12 21:46:07 -0500

    Relación Continente -> Ciudad

--- continents/models.py
+++ continents/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Continent(models.Model):
+  name = models.CharField(max_length=255)
+  translate = models.CharField(max_length=255)
+  color = models.CharField(max_length=255)
