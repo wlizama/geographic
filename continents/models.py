f7d1116ce96a97339d95d8c0cd9bc6d44f7fe654
Wilder Lizama <wilderlizama@gmail.com>
2018-04-16 22:40:10 -0500

    Creación de un Custom Manager

--- continents/models.py
+++ continents/models.py
@@ -6,3 +6,6 @@ class Continent(models.Model):
   name = models.CharField(max_length=255)
   translate = models.CharField(max_length=255)
   color = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
--- countries/models.py
+++ countries/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class ActiveManager(models.Manager):
+  
+  def get_query_set(self):
+    return suoper().get_queryset().filter(active=True)
+
+
 # Create your models here.
 class Country(models.Model):
   CODE_CHOICES = (
@@ -13,6 +19,9 @@ class Country(models.Model):
   name = models.CharField(max_length=255)    
   code = models.CharField(max_length=3, choices=CODE_CHOICES)
   continent = models.ForeignKey('continents.Continent', on_delete=models.CASCADE)
+  active = models.BooleanField(default=True)
+  active_manager = ActiveManager()
+  objects = models.Manager() # conservar el object manager por defecto
 
   def __str__(self):
     return self.name
