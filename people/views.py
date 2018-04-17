417fb90d73e085f957662288c320e86be127f66b
Wilder Lizama <wilderlizama@gmail.com>
2018-04-16 23:36:12 -0500

    Validación People form

--- people/views.py
+++ people/views.py
@@ -3,9 +3,10 @@ from django.http import JsonResponse
 from people.forms import RegisterForm
 
 def register(request):
-  form = RegisterForm()
+  form = RegisterForm(request.POST or None)
 
-  if request.method == "GET":
-    return render(request, "people/register.html", { "form" : form })
+  if form.is_valid():
+    return JsonResponse(form.cleaned_data)
 
-  return JsonResponse(request.POST)
+  return render(request, "people/register.html", { "form" : form })
+
